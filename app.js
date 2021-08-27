925aa5b2b563c7505e41059e46e2a9f22a0cc2d0
EstebanGZ91 <c215701@gmail.com>
2021-08-27 02:15:49 -0300

    more basics settings

--- app.js
+++ app.js
@@ -5,13 +5,19 @@ const path = require("path");
 const session = require("express-session");
 const cookies = require("cookie-parser");
 const methodOverride = require("method-override");
+const mainRoutes = require("./src/routes/mainRoutes");
+
 
 // localhost //
 app.listen(process.env.PORT || 3000, function(){
     console.log("Server running in port 3000");
 });
 
+// routes //
+app.use(express.static(path.join(__dirname, "./public")));
+app.use("/", mainRoutes);
 
 // views //
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+

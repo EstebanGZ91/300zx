befe672528b8873e3082d429740e08ce080dc9dc
EstebanGZ91 <c215701@gmail.com>
2021-08-29 00:54:00 -0300

    added mdb

---
+++ app.js
@@ -0,0 +1,25 @@
+// >>Requires<< //
+const express = require('express');
+const app = express();
+const methodOverride =  require('method-override');
+const path = require('path');
+const session = require('express-session');
+const cookies = require('cookie-parser');
+const mainRouter = require('./src/routes/mainRouter')
+// >>Localhost<< //
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Server running on port 3000');
+});
+
+// >>Views<< //
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// >>Routes<< //
+app.use('/', mainRouter);
+//app.use('/products', productsRoute);
+//app.use('/users', usersRoute);
+
+// >><< //
+app.use(express.static(path.join(__dirname,'./public')));
+app.use(methodOverride('_method'));

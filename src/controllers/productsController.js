0cd9e135718da4534bf8129fc7eb860b99ea574f
EstebanGZ91 <c215701@gmail.com>
2021-08-30 18:31:52 -0300

    added views

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -10,7 +10,26 @@ const productsController = {
     },
     detail: (req,res) => {
         res.render('./products/detail');
+    },
+    accessories: (req,res) => {
+        res.render('./products/acces');
+    },
+    amplifiers: (req,res) => {
+        res.render('./products/amps');
+    },
+    bass: (req,res) => {
+        res.render('./products/bass');
+    },
+    fx: (req,res) => {
+        res.render('./products/fxs');
+    },
+    guitars: (req,res) => {
+        res.render('./products/guitars')
+    },
+    vintage: (req,res) => {
+        res.render('./products/vintage')
     }
+
 }
 
 module.exports = productsController;
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -5,5 +5,11 @@ const productsController = require('./../controllers/productsController');
 router.get('/', productsController.detail);
 router.get('/edit', productsController.edit);
 router.get('/create', productsController.create);
+router.get('/acc', productsController.accessories);
+router.get('/amps', productsController.amplifiers);
+router.get('/bass', productsController.bass);
+router.get('/fx', productsController.fx);
+router.get('/guitars', productsController.guitars);
+router.get('/vintage', productsController.vintage);
 
 module.exports = router;
